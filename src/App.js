6f2e12cc57d5749df0596bb6afee082b2bdf1020
TAnhpika <anhsheeran12345@gmail.com>
2026-05-14 14:04:38 +0700

    Ex4

--- src/App.js
+++ src/App.js
@@ -3,20 +3,9 @@ import "./App.css";
 function App() {
     return (
         <div className="container">
-            <nav className="navbar">
-                <a href="#home" className="nav-item active">
-                    Home
-                </a> 
-                <a href="#search" className="nav-item">
-                    Search
-                </a>
-                <a href="#contact" className="nav-item">
-                    Contact
-                </a>
-                <a href="#login" className="nav-item login">
-                    Login
-                </a>
-            </nav>
+            <div className="jsx-box">
+                <span className="jsx-text">This is JSX</span>
+            </div>
         </div>
     );
 }
