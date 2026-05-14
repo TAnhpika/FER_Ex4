7b6a6ff62816be97ba2b22a0ca61afc2393f2b85
TAnhpika <anhsheeran12345@gmail.com>
2026-05-14 14:03:30 +0700

    Ex3

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="container">
-      <div className="card">
-        <img src={logo} alt="React Logo" className="react-logo" />
-        <hr className="divider" />
-        <h2 className="title">This is the React logo!</h2>
-        <p className="subtitle">(I don't know why it is here either)</p>
-        <p className="description">The library for web and native user interfaces</p>
-      </div>
-    </div>
-  );
+    return (
+        <div className="container">
+            <nav className="navbar">
+                <a href="#home" className="nav-item active">
+                    Home
+                </a> 
+                <a href="#search" className="nav-item">
+                    Search
+                </a>
+                <a href="#contact" className="nav-item">
+                    Contact
+                </a>
+                <a href="#login" className="nav-item login">
+                    Login
+                </a>
+            </nav>
+        </div>
+    );
 }
 
 export default App;
